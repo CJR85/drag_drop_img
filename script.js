81bfa77888bb7563fc81e9690f10bc1b99281b71
Calvin Roberts <roberts.calvin85@gmail.com>
2023-08-22 21:07:26 +0100

    Create function to upload images

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+const dropArea = document.getElementById('drop-area'),
+  inputFile = document.getElementById('input-file'),
+  imageView = document.getElementById('img-view');
+
+inputFile.addEventListener('change', uploadImage);
+
+function uploadImage() {
+  inputFile.files[0];
+  let imageLink = URL.createObjectURL(inputFile.files[0]);
+  imageView.style.backgroundImage = `url("${imageLink}")`;
+  imageView.textContent = '';
+  imageView.style.border = 0;
+}
